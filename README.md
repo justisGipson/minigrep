1d8ad5bbc930c4569dbb74805761f920d36b1d74
Justis <justis710@gmail.com>
2021-05-28 15:10:00 -0400

    minigrep done, output to be collected in .txt files

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Basic `grep` command line tool example.
 `grep` (**G**lobally search a **R**egular **E**xpression and **P**rint)
 
  In the simplest use case, grep searches a specified file for a specified string. To do so, grep takes as its arguments a filename and a string. Then it reads the file, finds lines in that file that contain the string argument, and prints those lines.
+
+ `cargo run to poem.txt` to find matches to the word `to` or make it whatever...
+
+ `cargo run to poem.txt > output.txt` for search results in a .txt file, instead of the console
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,43 @@
 use std::fs;
+use std::env;
 use std::error::Error;
 
 pub struct Config {
   pub query: String,
   pub filename: String,
+  pub case_sensitive: bool,
 }
 
 impl Config {
-  pub fn new(args: &[String]) ->  Result<Config, &str> {
-      if args.len() < 3 {
-          return Err("not enough arguments");
-      }
-      let query = args[1].clone();
-      let filename = args[2].clone();
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-      Ok(Config { query, filename })
-  }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.filename)?;
 
-  for line in search(&config.query, &contents) {
-    println!("{}", line);
+  let results = if config.case_sensitive {
+      search(&config.query, &contents)
+  } else {
+      search_case_insensitive(&config.query, &contents)
+  };
+
+  for line in results {
+      println!("{}", line);
   }
 
   Ok(())
@@ -35,6 +50,22 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
       if line.contains(query) {
           results.push(line);
       }
+  }
+
+  results
+}
+
+pub fn search_case_insensitive<'a> (
+  query: &str,
+  contents: &'a str,
+) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      results.push(line);
+    }
   }
 
   results
